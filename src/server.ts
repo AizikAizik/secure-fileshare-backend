829927e07f7c93b08afacc462f48281c81e3e0eb
AizikAizik <isaac.ogunleye@hotmail.com>
2025-07-25 23:01:21 +0100

    Initial project setup: Add dependencies, tsconfig, env, and folder structure

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+//import { dbConnect } from "./config/db";
+
+dotenv.config();
+
+const app: Express = express();
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Backend is running");
+});
+
+//dbConnect();
+
+const PORT: number = parseInt(process.env.PORT as string, 10) || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
